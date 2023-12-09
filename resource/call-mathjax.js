e8fea87a3f40b644e0834848d4c969ff1c0b2e38
Jason Coleman <jason@jasoncoleman.net>
2023-12-08 23:10:16 -0600

    Add simple example and working JS
    Proof-of-concept JS using command line argument to produce SVG.

--- resource/call-mathjax.js
+++ resource/call-mathjax.js
@@ -1,12 +1,18 @@
-// mini example at 
-// https://github.com/mathjax/MathJax#using-mathjax-components-in-a-node-application
-// adaptor.outerHTMLT produces <mjx-container> XML element output in console.
-// using adaptor.innerHTML just produces SVG content with no wrapper
+// this still needs to actually parse the .mml file XML data; and then write the
+// root <math> node structure to a string. Currently, if there is an <?xml> declaration
+// or a Doctype, it fails.
+
+// usage: node call-mathjax.js simple.mml
+
+var input = process.argv[2];
+
+const fs = require('node:fs');
+const xml = fs.readFileSync(input, 'utf8');
 
 require('mathjax').init({
-    loader: {load: ['input/tex', 'output/svg'],
+    loader: {load: ['input/mml', 'output/svg'],
     container: false}
   }).then((MathJax) => {
-    const svg = MathJax.tex2svg('\\frac{1}{x^2-1}', {display: true});
-    console.log(MathJax.startup.adaptor.innerHTML(svg));
+    const svg = MathJax.mathml2svg(xml, {display: true});
+    fs.writeFileSync('simple.svg', MathJax.startup.adaptor.innerHTML(svg),'utf8');
   }).catch((err) => console.log(err.message));
